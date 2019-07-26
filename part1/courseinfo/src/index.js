ecd83062c3556c6cd1769f77c904b71bb341369a
Ivaylo Petrov <ivaylop@starsgroup.com>
2019-07-26 11:15:51 +0300

    1.2 commit

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Part = (props) => (
+    <p>
+        {props.part} {props.excercise}
+    </p>
+);
+
 const Header = (props) => (
     <h1>{props.course}</h1>
 );
@@ -9,15 +15,9 @@ const Content = (props) => {
     const {part1, part2, part3, exercises1, exercises2, exercises3} = props.content;
     return (
         <>
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
+            <Part part={part1} excercise={exercises1} />
+            <Part part={part2} excercise={exercises2} />
+            <Part part={part3} excercise={exercises3} />
         </>
     )
 };
