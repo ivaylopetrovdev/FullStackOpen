d07a5b3fab92a88b9285a7eb1ac2207447e6ccba
Ivaylo Petrov <ivaylop@starsgroup.com>
2019-07-29 15:23:47 +0300

    1.13-1.14

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -3,13 +3,29 @@ import ReactDOM from 'react-dom'
 
 const App = (props) => {
     const [selected, setSelected] = useState(0);
+    const [selectedVotes, setSelectedVote] = useState(Array(anecdotes.length).fill(0));
+
+    const voteClick = (selectedAnecdote) => {
+        setSelectedVote(selectedVotes.map((value, index) => (index === selectedAnecdote ? value += 1 : value)));
+    };
+
+    const mostVotedIndex = selectedVotes.indexOf(Math.max(...selectedVotes));
 
     return (
         <div>
-            <p>{props.anecdotes[selected]}</p>
+            <h2>Anecdote of the day</h2>
+            <div>{props.anecdotes[selected]}</div>
+            <div>{`has ${selectedVotes[selected]} votes`}</div>
+            <button onClick={() => voteClick(selected)}>
+                vote
+            </button>
             <button onClick={() => setSelected(Math.floor(Math.random()*props.anecdotes.length))}>
                 next anecdote
             </button>
+
+            <h2>Anecdote with most votes</h2>
+            <div>{props.anecdotes[mostVotedIndex]}</div>
+            <div>{`has ${selectedVotes[mostVotedIndex]} votes`}</div>
         </div>
     )
 }
