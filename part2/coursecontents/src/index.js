bfdbd4b90cfdd8c1131c7131a8d067ffa390a8fc
Ivaylo Petrov <ivaylop@starsgroup.com>
2019-07-31 09:08:23 +0300

    2.3

--- part2/coursecontents/src/index.js
+++ part2/coursecontents/src/index.js
@@ -37,12 +37,8 @@ const Content = (props) => {
 };
 
 const Total = (props) => {
-    let total = 0;
     const {parts} = props;
-
-    parts.forEach((part) => (
-        total += part.exercises
-    ));
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0);
 
     return (<strong>total of {total} exercises</strong>);
 };
