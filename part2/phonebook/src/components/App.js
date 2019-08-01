e8efa14224142e5d6b633a060f2b9a749f0a16e6
Ivaylo Petrov <ivaylop@starsgroup.com>
2019-08-01 15:22:32 +0300

    2.16

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import personsService from '../services/persons'
 import Filter from './Filter';
 import PersonForm from './PersonForm';
 import Persons from './Persons';
@@ -36,20 +36,20 @@ const App = () => {
             number: newNumber
         };
 
-        axios
-            .post('http://localhost:3001/persons', newPerson)
-            .then(response => {
-                setPersons(persons.concat(response.data));
+        personsService
+            .create(newPerson)
+            .then(returnedPerson => {
+                setPersons(persons.concat(returnedPerson));
                 setNewName('');
                 setNewNumber('');
             })
     };
 
     useEffect(() => {
-        axios
-            .get('http://localhost:3001/persons')
-            .then(response => {
-                setPersons(response.data)
+        personsService
+            .getAll()
+            .then(allPersons => {
+                setPersons(allPersons)
             })
     }, []);
 
